4f600b9697a39f6b9c1488c5d1566cc304e02eee
Hugh Perkins <hughperkins@gmail.com>
2014-12-26 17:01:48 +0800

    added testfloatarray

---
+++ test/testfloatarray.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+#include "OpenCLHelper.h"
+#include "CLKernel.h"
+
+#include "test/asserts.h"
+
+int main( int argc, char *argv[] ) {
+    if( !OpenCLHelper::isOpenCLAvailable() ) {
+        cout << "opencl library not found" << endl;
+        exit(-1);
+    }
+    cout << "found opencl library" << endl;
+
+    OpenCLHelper cl;
+    CLKernel *kernel = cl.buildKernel("../test/testfloatarray.cl", "test");
+    float in[5];
+    float inout[5];
+    float out[5];
+    for( int i = 0; i < 5; i++ ) {
+        in[i] = i * 3;
+        inout[i] = i * 3;
+    }
+    kernel->in( 5, in );
+    kernel->out( 5, out );
+    kernel->inout( 5, inout );
+    kernel->run_1d( 5, 5 );
+
+    for( int i = 0; i < 5; i++ ) {
+        cout << out[i] << " ";
+    }
+    cout << endl;
+    for( int i = 0; i < 5; i++ ) {
+        cout << inout[i] << " ";
+    }
+    cout << endl;
+
+    assertEquals( inout[0], 7 );
+    assertEquals( inout[1] , 10 );
+    assertEquals( inout[2] , 34 );
+    assertEquals( inout[3] , 16 );
+    assertEquals( inout[4], 19 );
+    assertEquals( out[0] , 5 );
+    assertEquals( out[1] , 8 );
+    assertEquals( out[2] , 26 );
+    assertEquals( out[3] , 14 );
+    assertEquals( out[4] , 17 );
+    cout << "tests completed ok" << endl;
+}
+
