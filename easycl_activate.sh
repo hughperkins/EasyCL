900803c05a1e3f41109797dce708bfcc1e5b9cc2
Hugh Perkins <hughperkins@gmail.com>
2015-09-10 13:05:22 +0800

    use dyld_library_path on mac

--- easycl_activate.sh
+++ easycl_activate.sh
@@ -5,6 +5,9 @@ dist_dir=$(dirname ${bin_dir})
 
 export PATH=${bin_dir}:$PATH
 export LD_LIBRARY_PATH=${dist_dir}/lib:${LD_LIBRARY_PATH}
+if [[ $(uname) == Darwin ]]; then {
+    export DYLD_LIBRARY_PATH=${dist_dir}/lib:${DYLD_LIBRARY_PATH}
+} fi
 export CPATH=${dist_dir}/include:${dist_dir}/include/easycl
 export LDFLAGS="-L${dist_dir}/lib ${LDFLAGS}"
 
