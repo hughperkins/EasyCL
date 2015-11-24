42b492766032d5e481f6182577ab22309006e0e3
Hugh Perkins <hughperkins@gmail.com>
2015-11-24 18:55:10 +0800

    migrate some tests to stringify kernels

--- test/testbuildlog.cpp
+++ test/testbuildlog.cpp
@@ -6,14 +6,32 @@
 #include <stdexcept>
 using namespace std;
 
+static const char *getKernel();
+
 TEST(testbuildlog, main) {
     EasyCL *cl = EasyCL::createForFirstGpuOtherwiseCpu();
     try {
-        cl->buildKernel("testbuildlog.cl", "foo");
+        cl->buildKernelFromString(getKernel(), "foo", "", "testbuildlog.cl");
     } catch(std::runtime_error err) {
         cout << err.what() << endl;
         EXPECT_TRUE(string(err.what()).find("testbuildlog.cl") != string::npos);
     }
     delete cl;
+}
+
+static const char *getKernel() {
+    // [[[cog
+    // import stringify
+    // stringify.stringify("source", "test/testbuildlog.cl")
+    // ]]]
+    // generated using cog, from test/testbuildlog.cl:
+    const char * source =  
+    "kernel void foo() {\n" 
+    "   someerrorxyz;\n" 
+    "}\n" 
+    "\n" 
+    "";
+    // [[[end]]]
+    return source;
 }
 
